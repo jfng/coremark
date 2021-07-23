48ecf688270187812e2adbaf56f1d119d49ed88f
Jean-François Nguyen <jf@lambdaconcept.com>
2021-07-23 20:10:13 +0200

    litex_riscv32: add toolchain option and use 125MHz as default frequency.

--- build_litex_riscv32.py
+++ build_litex_riscv32.py
@@ -3,11 +3,13 @@ import argparse
 import subprocess
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--toolchain",      type=str, default="vivado")
+parser.add_argument("--output-dir",     type=str, default="build")
 parser.add_argument("--cpu-type",       type=str, default="vexriscv")
 parser.add_argument("--cpu-variant",    type=str, default="standard")
-parser.add_argument("--target",         type=str)
-parser.add_argument("--litex-src-path", type=str)
-parser.add_argument("--frequency",      type=float, default=100e6)
+parser.add_argument("--target",         type=str, default="digilent_arty")
+parser.add_argument("--litex-src-path", type=str, required=True)
+parser.add_argument("--frequency",      type=float, default=125e6)
 parser.add_argument("--iterations",     type=int, default=3000)
 parser.add_argument("--no-build-soc",   action="store_true", default=False)
 parser.add_argument("--no-build-bench", action="store_true", default=False)
@@ -15,10 +17,11 @@ parser.add_argument("--no-program",     action="store_true", default=False)
 args = parser.parse_args()
 
 litex_build = f"python -m litex_boards.targets.{args.target} \
+--toolchain={args.toolchain} \
 --sys-clk-freq={args.frequency} \
 --cpu-type={args.cpu_type} \
 --cpu-variant={args.cpu_variant} \
---output-dir=build/{args.target}"""
+--output-dir={args.output_dir}/{args.target}"
 
 if not args.no_build_soc:
     cmd = f"{litex_build} --build"
@@ -30,8 +33,8 @@ if not args.no_build_bench:
 PORT_DIR=litex_riscv32 \
 BUILD_DIR=build/coremark/ \
 LITEX_GEN_DIR=build/{args.target}/software/include \
-LITEX_CPU_DIR={args.litex_src_path}/soc/cores/cpu/{args.cpu_type} \
-LITEX_INC_DIR={args.litex_src_path}/soc/software/include \
+LITEX_CPU_DIR={args.litex_src_path}/litex/soc/cores/cpu/{args.cpu_type} \
+LITEX_INC_DIR={args.litex_src_path}/litex/soc/software/include \
 XCFLAGS="-DCLOCKS_PER_SEC={args.frequency}" \
 ITERATIONS={args.iterations} \
 MULDIV=yes \
